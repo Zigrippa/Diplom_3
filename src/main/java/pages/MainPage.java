6a721413980c310469c6a40a2832004bc0ac971e
Zigrippa <alter0028@gmail.com>
2023-05-09 16:14:37 +0300

    Modified ConstructorTest.java and MainPage.java.
    All tests work

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -17,13 +17,17 @@ public class MainPage {
     //локаторы
     private final By enterToAccountButton = By.xpath("/html/body/div/div/main/section[2]/div/button");
     private final By personalAreaButton = By.xpath("/html/body/div/div/header/nav/a");
-
     private final By checkoutOrderButton = By.xpath("/html/body/div/div/main/section[2]/div/button");
-
     private final By stellarisBurgerLogo = By.xpath("/html/body/div/div/header/nav/div/a/svg");
 
 
+    private final By bunsSelectionButton = By.xpath("//*[@id=\"root\"]/div/main/section[1]/div[1]/div[1]");
+    private final By sousesSelectionButton = By.xpath("//*[@id=\"root\"]/div/main/section[1]/div[1]/div[2]");
+    private final By fillingsSelectionButton = By.xpath("//*[@id=\"root\"]/div/main/section[1]/div[1]/div[3]");
 
+    private final By bunsSelection = By.xpath("//*[@id=\"root\"]/div/main/section[1]/div[2]/h2[1]");
+    private final By sousesSelection = By.xpath("//*[@id=\"root\"]/div/main/section[1]/div[2]/h2[2]");
+    private final By fillingsSelection = By.xpath("//*[@id=\"root\"]/div/main/section[1]/div[2]/h2[3]");
 
 
     //методы
@@ -45,6 +49,39 @@ public class MainPage {
     public MainPage clickStellarisBurgerLogo() {
         driver.findElement(stellarisBurgerLogo).click();
         return new MainPage(driver);
+    }
+
+    public MainPage clickBunsSelectionButton() {
+        driver.findElement(bunsSelectionButton).click();
+        return new MainPage(driver);
+    }
+
+    public MainPage clickSousesSelectionButton() {
+        driver.findElement(sousesSelectionButton).click();
+        return new MainPage(driver);
+    }
+
+    public MainPage clickFillingsSelectionButton() {
+        driver.findElement(fillingsSelectionButton).click();
+        return new MainPage(driver);
+    }
+
+    public boolean isBunsVisible() {
+        return driver.findElement(bunsSelection).isDisplayed() && driver.findElement(bunsSelectionButton)
+                .getAttribute("class")
+                .contains("tab_tab_type_current__2BEPc");
+    }
+
+    public boolean isSousesVisible() {
+        return driver.findElement(sousesSelection).isDisplayed() && driver.findElement(sousesSelectionButton)
+                .getAttribute("class")
+                .contains("tab_tab_type_current__2BEPc");
+    }
+
+    public boolean isFillingsVisible() {
+        return driver.findElement(fillingsSelection).isDisplayed() && driver.findElement(fillingsSelectionButton)
+                .getAttribute("class")
+                .contains("tab_tab_type_current__2BEPc");
     }
 
 
--- src/test/java/ConstructorTest.java
+++ src/test/java/ConstructorTest.java
@@ -1,2 +1,68 @@
+import api_models.User;
+import api_models.UserClient;
+import config.ApiConfig;
+import io.restassured.RestAssured;
+import io.restassured.response.ValidatableResponse;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import pages.MainPage;
+import tools.WebDriverFactory;
+
+import static org.junit.Assert.assertTrue;
+
 public class ConstructorTest {
+
+        private WebDriver driver;
+        public UserClient userClient;
+        public User user;
+        public String accessToken;
+
+
+        @Before
+        public void setUp() {
+            user = new User().generateUser();
+            userClient = new UserClient();
+            RestAssured.baseURI = ApiConfig.BASE_URL;
+            driver = WebDriverFactory.get("chrome", "main");
+            userClient.create(user);
+        }
+
+        @After
+        public void tearDown() {
+            driver.quit();
+            if (accessToken != null) userClient.delete(accessTokenExtraction(user));
+        }
+
+        public String accessTokenExtraction(User user) {
+            ValidatableResponse response = userClient.login(user);
+            return response.extract().path("accessToken");
+        }
+
+        @Test
+        public void clickBunsSelectionButton() {
+            boolean isBunsSelected = new MainPage(driver)
+                    .clickFillingsSelectionButton()
+                    .clickBunsSelectionButton()
+                    .isBunsVisible();
+            assertTrue(isBunsSelected);
+        }
+
+    @Test
+    public void clickSousesSelectionButton() {
+        boolean isSousesSelected = new MainPage(driver)
+                .clickSousesSelectionButton()
+                .isSousesVisible();
+        assertTrue(isSousesSelected);
+    }
+
+    @Test
+    public void clickFillingsSelectionButton() {
+        boolean isFillingsSelected = new MainPage(driver)
+                .clickFillingsSelectionButton()
+                .isFillingsVisible();
+        assertTrue(isFillingsSelected);
+    }
+
 }
